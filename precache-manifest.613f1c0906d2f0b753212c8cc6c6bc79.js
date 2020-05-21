2efbf938c641be17d7777ceb40a4a1b4bd2491ca
Sam <sam.carey312@gmail.com>
2020-05-21 14:36:40 -0500

    Updates

---
+++ precache-manifest.613f1c0906d2f0b753212c8cc6c6bc79.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "306916e48184a7c978f0290d34faa2b4",
+    "url": "/sam-c-portfolio/index.html"
+  },
+  {
+    "revision": "744801ab72d57c07ba75",
+    "url": "/sam-c-portfolio/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "07038df4ec2e79655e54",
+    "url": "/sam-c-portfolio/static/css/main.f8133773.chunk.css"
+  },
+  {
+    "revision": "744801ab72d57c07ba75",
+    "url": "/sam-c-portfolio/static/js/2.43283e74.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/sam-c-portfolio/static/js/2.43283e74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07038df4ec2e79655e54",
+    "url": "/sam-c-portfolio/static/js/main.fec49748.chunk.js"
+  },
+  {
+    "revision": "630e9c6319f51123556a",
+    "url": "/sam-c-portfolio/static/js/runtime-main.6caa4dd3.js"
+  },
+  {
+    "revision": "742045a0d7334ba0e17720d30749bb61",
+    "url": "/sam-c-portfolio/static/media/samProf.742045a0.jpg"
+  }
+]);
